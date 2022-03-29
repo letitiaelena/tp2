7d5042d46a51b8129aca2f26a92a28640efd15eb
letitiaelena <101173971+letitiaelena@users.noreply.github.com>
2022-03-29 11:07:36 +0300

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 def on_button_pressed_a():
-    pass
+    images.create_image("""
+        # . . . .
+                # . . . .
+                # . . . .
+                # . . . .
+                # # # . .
+    """).show_image(0)
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_b():
+    images.create_image("""
+        . . # # #
+                . # . . .
+                . . # # .
+                . . . . #
+                . # # # .
+    """).show_image(0)
+input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_forever():
     pass
--- main.ts
+++ main.ts
@@ -1,21 +1,27 @@
 input.onButtonPressed(Button.A, function () {
     images.createImage(`
-        # . . . .
-        # . . . .
-        # . . . .
-        # . . . .
-        # # # . .
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
         `).showImage(0)
 })
 input.onButtonPressed(Button.B, function () {
     images.createImage(`
-        . . # # #
-        . # . . .
-        . . # # .
-        . . . . #
+        . # . # .
+        . # . # .
+        . . . . .
         . # # # .
+        # . . . #
         `).showImage(0)
 })
 basic.forever(function () {
 	
+})
+basic.forever(function () {
+    pins.digitalWritePin(DigitalPin.P2, 0)
+    basic.pause(500)
+    pins.digitalWritePin(DigitalPin.P3, 1)
+    basic.pause(500)
 })
