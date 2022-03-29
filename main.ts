b1e46ff954ab967049525a1ef43bdb509a636764
letitiaelena <101173971+letitiaelena@users.noreply.github.com>
2022-03-29 11:42:28 +0300

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,27 +1,8 @@
-input.onButtonPressed(Button.A, function () {
-    images.createImage(`
-        . # . # .
-        . # . # .
-        . . . . .
-        # . . . #
-        . # # # .
-        `).showImage(0)
-})
-input.onButtonPressed(Button.B, function () {
-    images.createImage(`
-        . # . # .
-        . # . # .
-        . . . . .
-        . # # # .
-        # . . . #
-        `).showImage(0)
-})
+for (let i = 0; i <= 9; i++) {
+    if (i % 2 != 1) {
+        basic.showNumber(i)
+    }
+}
 basic.forever(function () {
 	
-})
-basic.forever(function () {
-    pins.digitalWritePin(DigitalPin.P2, 0)
-    basic.pause(500)
-    pins.digitalWritePin(DigitalPin.P3, 1)
-    basic.pause(500)
 })
